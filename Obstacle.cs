b5aaf12f1284ae066452e8d2804821bddf6ab69a
Fishii <80645813+Fishii1612@users.noreply.github.com>
2021-04-19 11:43:20 +0200

    Update Obstacle.cs

--- Obstacle.cs
+++ Obstacle.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,9 +13,17 @@ namespace Jump_or_die
     public class Obstacle
     {
         public int Position;
-        public Obstacle(int pos)
+        public Rectangle ObstacleHitbox;
+        private Texture2D texture1;
+        public Obstacle(int pos, Texture2D texture)
         {
             Position = pos;
+            texture1 = texture; 
+        }
+        public void Update()
+        {
+            Position--;
+            ObstacleHitbox = new Rectangle(Position, 300 - texture1.Height, texture1.Width, texture1.Height);
         }
     }
 }
